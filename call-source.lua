849194508ecd5ee8e1ec2ce6f275359db28c4cd9
Jiri Vanek <jvanek@redhat.com>
2017-01-19 13:44:46 +0100

    Added file used for sourcing the main logic in pretrans scripelts

---
+++ call-source.lua
@@ -0,0 +1,33 @@
+#!/usr/bin/lua
+local posix = require "posix"
+
+local debug = true
+SOURCE1 = "/home/jvanek/hg/copy_jdk_configs/copy_jdk_configs.lua"
+SOURCE2 = "/usr/libexec/copy_jdk_configs.lua"
+
+local stat1 = posix.stat(SOURCE1, "type");
+local stat2 = posix.stat(SOURCE2, "type");
+
+  if (stat1 ~= nil) then
+  if (debug) then
+    print(SOURCE1 .." exists - copy-jdk-configs in transaction, using this one.")
+  end;
+  package.path = package.path .. ";" .. SOURCE1
+else 
+  if (stat2 ~= nil) then
+  if (debug) then
+    print(SOURCE2 .." exists - copy-jdk-configs alrady installed and NOT in transation. Using.")
+  end;
+  package.path = package.path .. ";" .. SOURCE2
+  else
+    if (debug) then
+      print(SOURCE1 .." does NOT exists")
+      print(SOURCE2 .." does NOT exists")
+      print("No config files will be copied")
+    end
+  os.exit(0)
+  end
+end
+-- run contetn of included file
+arg = {"--debug true"}
+require "copy_jdk_configs.lua"
